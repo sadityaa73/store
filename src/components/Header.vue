158fc9c35d3ba29df1f832cbf76272acad7d2b26
Aditya singh <sadityaa73@gmail.com>
2022-02-15 17:46:27 +0530

    loginHover

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,9 @@
         />
         <span class="fas fa-search"></span>
       </div>
-      <div id="btn-Login">Login</div>
+      <div id="btn-comp">
+        <div id="btn-Login" @click="hover">{{ hover ? "" : "" }}Login</div>
+      </div>
       <div id="more">More</div>
       <div id="cart">
         <span class="fas fa-cart-shopping"></span>
@@ -18,11 +20,41 @@
       </div>
     </div>
   </div>
+  <div v-if="HoverOnLogin" class="list">
+    <ul class="loginExtn">
+      <li>
+        <div class="login-head">
+          <span class="h1">New Customer ?</span>
+          <span class="h2">Sign Up</span>
+        </div>
+      </li>
+      <li class="Profile">
+        <span class="fas fa-user"></span>
+        <h2>My Profile</h2>
+      </li>
+      <li class="Orders">
+        <span class="fas fa-bag-shopping"></span>
+        <h2>Orders</h2>
+      </li>
+      <li class="WishList">
+        <span class="fas fa-cart-arrow-down"></span>
+        <h2>Wishlist</h2>
+      </li>
+    </ul>
+  </div>
 </template>
 <script scoped>
 export default {
+  components: {},
   data: function () {
-    return {};
+    return {
+      HoverOnLogin: "",
+    };
+  },
+  methods: {
+    hover() {
+      this.HoverOnLogin = !this.HoverOnLogin;
+    },
   },
 };
 </script>
@@ -32,7 +64,6 @@ export default {
   justify-content: center;
   align-items: center;
   background: skyblue;
-  height: 70px;
 }
 #headerStrip {
   display: flex;
@@ -52,6 +83,7 @@ export default {
   align-content: center;
   justify-content: center;
   width: 610px;
+  height: 45px;
   border: 1px solid;
   background-color: green;
   box-shadow: 2px 2px 28px 5px;
@@ -65,7 +97,7 @@ export default {
 }
 #btn-Login {
   border: 1px solid;
-  margin-left: 26px;
+  margin-left: 61px;
   width: 123px;
   height: 34px;
   margin-top: 6px;
@@ -111,5 +143,74 @@ export default {
   font-size: 20px;
   font-family: serif;
   margin-left: 9px;
+}
+.list {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid;
+  width: 263px;
+  margin-left: 817px;
+  padding: 0px;
+}
+.loginExtn {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding: 0px;
+}
+.h1 {
+  margin-right: 62px;
+  margin-left: 14px;
+  font-size: 17px;
+  font-family: sans-serif;
+}
+.h2 {
+  margin-left: -15px;
+  color: blue;
+  font-size: 17px;
+  font-family: sans-serif;
+}
+.fa-user {
+  font-size: 20px;
+  margin-left: 16px;
+  margin-top: 9px;
+}
+.fa-bag-shopping {
+  font-size: 20px;
+  margin-left: 16px;
+  margin-top: 9px;
+}
+.fa-cart-arrow-down {
+  font-size: 20px;
+  margin-left: 16px;
+  margin-top: 9px;
+}
+.Profile {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.Profile h2 {
+  margin-left: 11px;
+  font-family: sans-serif;
+  font-size: 14px;
+}
+.Orders {
+  display: flex;
+}
+.Orders h2 {
+  margin-left: 11px;
+  font-family: sans-serif;
+  font-size: 14px;
+}
+.WishList {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.WishList h2 {
+  margin-left: 11px;
+  font-family: sans-serif;
+  font-size: 14px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import Header from "./components/Header.vue"
+import moreExtn from "./components/HeaderComp/moreExtn.vue"
 import App from './App.vue'
 
 const app = createApp(App);
 app.component('Header', Header)
+app.component('moreExtn', moreExtn)
 
 createApp(App).mount('#app')
